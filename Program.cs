874027b2948f6357e2603d6c2e97799e3c6a6f85
Andre Felipe <andrefelipe.pi2@gmail.com>
2023-02-28 22:31:44 -0300

    Estrutura repetitiva for

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace In_Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int N = int.Parse(Console.ReadLine());
+            int[] vet = new int[N];
+
+            int contin = 0;
+            int contout = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = int.Parse(Console.ReadLine());
+                if (vet[i] >= 10 && vet[i] <= 20)
+                {
+                    contin = contin + 1;
+                }
+                else
+                {
+                    contout = contout + 1;
+                }
+            }
+
+            Console.WriteLine(contin + " in");
+            Console.WriteLine(contout + " out");
+        }
+    }
+}
